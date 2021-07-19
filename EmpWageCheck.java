1f5df16f51d560042edce2df40a608e2e94cb285
SW-GAGAN <srgagan73@gmail.com>
2021-07-19 07:59:57 +0530

    Adding: EWmployee Present Absent

--- EmpWageCheck.java
+++ EmpWageCheck.java
@@ -1,5 +1,25 @@
 public class EmpWageCheck {
-    public static void main(String[] args){
-            System.out.println("Welcome employee wage computation problem");
+    // Creating a method
+    public void welcomeMsg() {
+        System.out.println("Welcome employee wage computation problem");
+
+        // Check the employee is present or absent
+        int IS_FULL_TIME = 1;
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+        if (empcheck == IS_FULL_TIME) {
+            System.out.println("emp is present");
+        } else {
+            System.out.println("emp is not present");
+        }
+
     }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        // To creating a object and call the method
+        EmpWageCheck ewc = new EmpWageCheck();
+        ewc.welcomeMsg();
+
+    }
+
 }

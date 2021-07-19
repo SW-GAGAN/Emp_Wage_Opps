35bbc0c3cfe5b8b0bd7cfb8c62c2845f59f133b0
SW-GAGAN <srgagan73@gmail.com>
2021-07-19 08:26:37 +0530

    Adding: EWmployee Wage calculation for month

--- EmpWageCheck.java
+++ EmpWageCheck.java
@@ -4,6 +4,8 @@ public class EmpWageCheck {
         // Check the employee is present or absent
         final int Is_Part_Time = 1; // Added part time condition
         final int IS_FULL_TIME = 2;
+
+        final int NoOfWorkingDays = 2;
         // Calculating daily employee wage
         final int empWagePerHrs = 20;
         int empHrs = 0;
@@ -33,12 +35,44 @@ public class EmpWageCheck {
         System.out.println("Emp Wage"+ " "+ empWage);
 
     }
+    public void empWage() {
+        final int IsPartTime = 1;
+        final int IsFullTime = 2;
+        final int EmpRatePerHrs = 20;
+        final int NoOfWorkingDays = 2;
 
+        int EmpWage = 0;
+        int Emphrs = 0;
+        int totalEmpWage = 0;
+        for (int days = 0; days < NoOfWorkingDays; days++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IsPartTime:
+                    Emphrs = 4;
+                    break;
+                case IsFullTime:
+                    Emphrs = 8;
+                    break;
+                default:
+                    Emphrs = 0;
+            }
+            EmpWage = Emphrs * EmpRatePerHrs;
+            totalEmpWage += EmpWage;
+            System.out.println(" ");
+            System.out.println("Emp Wage" + " " + EmpWage);
+        }
+        System.out.println("totalEmpWage" + " " + totalEmpWage);
+
+
+
+    }
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         // To creating a object and call the method
         EmpWageCheck ewc = new EmpWageCheck();
         ewc.welcomeMsg();
+        EmpWageCheck ewc1=new EmpWageCheck();
+        ewc1.empWage();
 
     }
 
